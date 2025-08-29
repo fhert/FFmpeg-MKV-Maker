f7d2740c9f1f8eb592e2cd358d40b0fca35dccbf
fhert <thescarecrow20@gmail.com>
2025-08-29 01:29:03 -0500

    Create Configuracion.kt

---
+++ app/src/main/java/com/z/ffmpegmkvmaker/Configuracion.kt
@@ -0,0 +1,9 @@
+package com.z.ffmpegmkvmaker
+
+data class Configuracion(
+    var resolucion: String = "",
+    var relacionAspecto: String = "",
+    var rangoDinamico: String = "",
+    var audioOpcion: String = "",
+    var subtitulosOpcion: String = ""
+)
